7a97e730d708cf9bf1fb377b68d6bbad154ec2b6
Yazid <yazid780@outlook.fr>
2024-05-27 11:19:47 +0200

    maj css cards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import '../scss/Footer.scss'
+import "../scss/Footer.scss";
 import LogoFooter from "../assets/LogoFooter.png";
 export function Footer() {
   return (
     <footer className="footer">
-      <img className="bannerText" src={LogoFooter} alt="Logo Kasa footer"  />
+      <img className="bannerText" src={LogoFooter} alt="Logo Kasa footer" />
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
