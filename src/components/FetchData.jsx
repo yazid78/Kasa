2bbb2e26522e81cd74cbf6a774cb4ff92b64560b
Yazid <yazid780@outlook.fr>
2024-06-01 22:12:09 +0200

    Fix path to logements.json file

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -3,9 +3,14 @@ import { useEffect, useState } from "react";
 export function useFetchData(url) {
   const [data, setData] = useState([]);
   
-  useEffect(() =>{
+  useEffect(() => {
     fetch(url)
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then(result => setData(result))
       .catch(err => console.error(err));
   }, [url]);
